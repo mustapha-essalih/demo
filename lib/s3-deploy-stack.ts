d9ca73462b2736e1ae8d9ba9ff56357c4c08af9f
mustapha-essalih <mustaphessalih1937@gmail.com>
2025-05-21 17:15:21 +0100

    .

--- lib/s3-deploy-stack.ts
+++ lib/s3-deploy-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
-export class S3DeployStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class S3DeployStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    // L1 Bucket
+    new s3.CfnBucket(this, 'MyL1Bucket', {
+      bucketName: `l1-bucket-unique-application-bucket-1210345`
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3DeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2 Bucket
+    new s3.Bucket(this, 'MyL2Bucket', {
+      bucketName: `l2-bucket-unique-application-bucket-1234510`,
+      encryption: s3.BucketEncryption.S3_MANAGED
+    });
   }
 }
